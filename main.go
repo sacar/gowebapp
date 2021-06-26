75d84498611d64abf53c014b24a077cec74b77d5
Sakar Ghimire <sakar@kathmandu>
2021-06-26 08:07:48 +0545

    a simple web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Arey tati tera na kya hai?</h1>")
+}
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
